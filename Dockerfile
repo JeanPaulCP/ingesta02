1992bf54fc16c5734aaa104403dd3fa1efcbc874
JeanPaulCP <140226335+JeanPaulCP@users.noreply.github.com>
2025-05-11 20:21:26 -0500

    Agrego los datos de la instancia EC2

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 FROM python:3-slim
 WORKDIR /programas/ingesta
 RUN pip3 install boto3
+RUN pip3 install mysql-connector-python
+RUN pip3 install pandas
 COPY . .
 CMD [ "python3", "./ingesta.py" ]
--- ingesta.py
+++ ingesta.py
@@ -1,9 +1,34 @@
+import mysql.connector
+import pandas as pd
 import boto3
 
-ficheroUpload = "data.csv"
-nombreBucket = "gcr-output-01"
+host = "98.81.227.242" # IP pública de la instancia EC2
+port = 8005
+user = "root"
+password = "utecmysql"
+database = "usuariosdb"
 
+# Conexión a la base de datos
+conn = mysql.connector.connect(
+    host=host,
+    port=port,
+    user=user,
+    password=password,
+    database=database
+)
+
+query = "SELECT * FROM usuarios"
+df = pd.read_sql(query, conn)
+conn.close()
+
+csv_filename = "usuarios.csv"
+df.to_csv(csv_filename, index=False)
+
+print("Exportación a CSV completa.")
+
+bucket_name = "ingesta-mysql"
 s3 = boto3.client('s3')
-response = s3.upload_file(ficheroUpload, nombreBucket, ficheroUpload)
 
-print("Ingesta completada")
+s3.upload_file(csv_filename, bucket_name, csv_filename)
+
+print("Ingesta completada en S3")
